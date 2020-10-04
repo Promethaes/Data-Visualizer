41994146ec772ffea67344724aa6ebc6083ab709
Anthony <anth19800@hotmail.com>
2020-10-04 04:50:18 -0400

    [Add] Init

---
+++ Assets/InputActions.cs
@@ -0,0 +1,231 @@
+// GENERATED AUTOMATICALLY FROM 'Assets/InputActions.inputactions'
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.Utilities;
+
+public class @InputActions : IInputActionCollection, IDisposable
+{
+    public InputActionAsset asset { get; }
+    public @InputActions()
+    {
+        asset = InputActionAsset.FromJson(@"{
+    ""name"": ""InputActions"",
+    ""maps"": [
+        {
+            ""name"": ""Default"",
+            ""id"": ""fa5406cc-019f-4bf3-968a-484aefc6f761"",
+            ""actions"": [
+                {
+                    ""name"": ""Movement"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""066a0a7c-91e2-4f9d-b05a-42caa61dc7a4"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                },
+                {
+                    ""name"": ""MouseInput"",
+                    ""type"": ""PassThrough"",
+                    ""id"": ""5faa5238-f47b-490b-9624-608a24573e72"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """"
+                }
+            ],
+            ""bindings"": [
+                {
+                    ""name"": """",
+                    ""id"": ""2829aefa-b1fa-43c2-b795-c302ab565e74"",
+                    ""path"": ""<Gamepad>/leftStick"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Default"",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""2D Vector"",
+                    ""id"": ""40301f56-4e73-4eb9-b5bf-5b087cfa5821"",
+                    ""path"": ""2DVector"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": true,
+                    ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": ""up"",
+                    ""id"": ""6100cf50-85c7-42bb-891b-d950b165af0d"",
+                    ""path"": ""<Keyboard>/w"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""down"",
+                    ""id"": ""e40785c2-761e-40ea-936c-0b9a956a453b"",
+                    ""path"": ""<Keyboard>/s"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""left"",
+                    ""id"": ""6b4af8a1-7edf-4ee4-baa3-5e8acece50fe"",
+                    ""path"": ""<Keyboard>/a"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": ""right"",
+                    ""id"": ""7ccb4e35-37c5-4f0b-a4e4-615ecad7311f"",
+                    ""path"": ""<Keyboard>/d"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Movement"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": true
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""06f0d71b-5b18-4b74-9c11-736b9af6999e"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": ""Default"",
+                    ""action"": ""MouseInput"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
+                }
+            ]
+        }
+    ],
+    ""controlSchemes"": [
+        {
+            ""name"": ""Default"",
+            ""bindingGroup"": ""Default"",
+            ""devices"": []
+        }
+    ]
+}");
+        // Default
+        m_Default = asset.FindActionMap("Default", throwIfNotFound: true);
+        m_Default_Movement = m_Default.FindAction("Movement", throwIfNotFound: true);
+        m_Default_MouseInput = m_Default.FindAction("MouseInput", throwIfNotFound: true);
+    }
+
+    public void Dispose()
+    {
+        UnityEngine.Object.Destroy(asset);
+    }
+
+    public InputBinding? bindingMask
+    {
+        get => asset.bindingMask;
+        set => asset.bindingMask = value;
+    }
+
+    public ReadOnlyArray<InputDevice>? devices
+    {
+        get => asset.devices;
+        set => asset.devices = value;
+    }
+
+    public ReadOnlyArray<InputControlScheme> controlSchemes => asset.controlSchemes;
+
+    public bool Contains(InputAction action)
+    {
+        return asset.Contains(action);
+    }
+
+    public IEnumerator<InputAction> GetEnumerator()
+    {
+        return asset.GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+
+    public void Enable()
+    {
+        asset.Enable();
+    }
+
+    public void Disable()
+    {
+        asset.Disable();
+    }
+
+    // Default
+    private readonly InputActionMap m_Default;
+    private IDefaultActions m_DefaultActionsCallbackInterface;
+    private readonly InputAction m_Default_Movement;
+    private readonly InputAction m_Default_MouseInput;
+    public struct DefaultActions
+    {
+        private @InputActions m_Wrapper;
+        public DefaultActions(@InputActions wrapper) { m_Wrapper = wrapper; }
+        public InputAction @Movement => m_Wrapper.m_Default_Movement;
+        public InputAction @MouseInput => m_Wrapper.m_Default_MouseInput;
+        public InputActionMap Get() { return m_Wrapper.m_Default; }
+        public void Enable() { Get().Enable(); }
+        public void Disable() { Get().Disable(); }
+        public bool enabled => Get().enabled;
+        public static implicit operator InputActionMap(DefaultActions set) { return set.Get(); }
+        public void SetCallbacks(IDefaultActions instance)
+        {
+            if (m_Wrapper.m_DefaultActionsCallbackInterface != null)
+            {
+                @Movement.started -= m_Wrapper.m_DefaultActionsCallbackInterface.OnMovement;
+                @Movement.performed -= m_Wrapper.m_DefaultActionsCallbackInterface.OnMovement;
+                @Movement.canceled -= m_Wrapper.m_DefaultActionsCallbackInterface.OnMovement;
+                @MouseInput.started -= m_Wrapper.m_DefaultActionsCallbackInterface.OnMouseInput;
+                @MouseInput.performed -= m_Wrapper.m_DefaultActionsCallbackInterface.OnMouseInput;
+                @MouseInput.canceled -= m_Wrapper.m_DefaultActionsCallbackInterface.OnMouseInput;
+            }
+            m_Wrapper.m_DefaultActionsCallbackInterface = instance;
+            if (instance != null)
+            {
+                @Movement.started += instance.OnMovement;
+                @Movement.performed += instance.OnMovement;
+                @Movement.canceled += instance.OnMovement;
+                @MouseInput.started += instance.OnMouseInput;
+                @MouseInput.performed += instance.OnMouseInput;
+                @MouseInput.canceled += instance.OnMouseInput;
+            }
+        }
+    }
+    public DefaultActions @Default => new DefaultActions(this);
+    private int m_DefaultSchemeIndex = -1;
+    public InputControlScheme DefaultScheme
+    {
+        get
+        {
+            if (m_DefaultSchemeIndex == -1) m_DefaultSchemeIndex = asset.FindControlSchemeIndex("Default");
+            return asset.controlSchemes[m_DefaultSchemeIndex];
+        }
+    }
+    public interface IDefaultActions
+    {
+        void OnMovement(InputAction.CallbackContext context);
+        void OnMouseInput(InputAction.CallbackContext context);
+    }
+}
---
+++ Assets/Scripts/PlayerInputScript.cs
@@ -0,0 +1,55 @@
+﻿
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInputScript : MonoBehaviour
+{
+    InputActions _inputActions;
+    public float moveSpeed;
+    public float jumpSpeed;
+    float _dashCooldown = 0.0f;
+    float _isDashing = 0.0f;
+    bool _dashed = false;
+    bool _airDashed = false;
+    float _jumpCooldown = 0.0f;
+    float _isJumping = 0.0f;
+    bool _jumped = false;
+    bool _doubleJumped = false;
+    Vector2 _moveInput = new Vector2(0.0f,0.0f);
+
+    float _isSprinting = 0.0f;
+
+    void Awake()
+    {
+        _inputActions = new InputActions();
+        _inputActions.Default.Movement.performed += ctx => _moveInput = ctx.ReadValue<Vector2>();
+        _inputActions.Default.Movement.canceled += ctx => _moveInput = ctx.ReadValue<Vector2>();
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //WASD Movement
+        Move(_moveInput*Time.deltaTime);
+
+        _jumpCooldown -= Time.deltaTime;
+        _dashCooldown -= Time.deltaTime;
+       
+    }
+
+    void Move(Vector2 _moveInput)
+    {
+        gameObject.transform.position = gameObject.transform.position +  new Vector3(_moveInput.x,_moveInput.y,0.0f) * moveSpeed;
+    }
+    void OnEnable()
+    {
+        _inputActions.Enable();
+    }
+
+    void OnDisable()
+    {
+        _inputActions.Disable();
+    }
+}
